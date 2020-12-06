4f5981c7ce43ff1c640c0a6b3e91ddf8718f3bca
Vincent Diep <vincent.diep61@gmail.com>
2020-12-06 05:16:22 -0500

    [golf] Reduce byte count by wasting memory and synchronously processing the file

--- day-3/golfed.cjs
+++ day-3/golfed.cjs
@@ -1,3 +1,4 @@
-(async _=>{b=await Promise.all([_(1),a=await _(3),_(5),_(7),_(1,2)])
-console.log(a,b.reduce((a,v)=>a*v))})(async(r,d=1,x=0,y=0,t=0,z=require)=>{for await(n of z('readline').createInterface({input:z('fs').createReadStream(__dirname+`/input.txt`)}))if(y++%d<1){t+=`#`==n[x%n.length]
-x+=r}return t})
+$=(r,d=1,x=0,y=0,t=0)=>require('fs').readFileSync(__dirname+`/input.txt`,{encoding:"utf8"}).split`
+`.map(n=>{y++%d<1?(t+=`#`==n[x%n.length],x+=r):0})&&t
+_=[$(1),$(3),$(5),$(7),$(1,2)]
+console.log(_[1],_.reduce((a,v)=>a*v))
