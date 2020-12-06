9239ab603ae7af166aaa8a34cc742e89f5cd8421
Vincent Diep <vincent.diep61@gmail.com>
2020-12-05 19:08:10 -0500

    [golf] transform into sorted array and use find and pop functions

--- day-5/golfed.cjs
+++ day-5/golfed.cjs
@@ -1,5 +1,2 @@
-s={}
-f=m=0
-require('fs').readFileSync(__dirname+`/input.txt`,{encoding:'utf8'}).trim().replace(/[FL]/g,0).replace(/[BR]/g,1).split`\n`.map(l=>{m=Math.max(m,d=eval('0b'+l))
-s[d]=1})
-for(i=1024;f=f||s[i],i--;)i=f&&!s[i]?console.log(m,i):i
+s=require('fs').readFileSync(__dirname+`/input.txt`,{encoding:`utf8`}).trim``.replace(/[FL]/g,0).replace(/[BR]/g,1).split`\n`.map(l=>eval('0b'+l)).sort((a,b)=>a-b)
+console.log(s.pop(),s.find((t,i)=>t-i-s[0])-1)
