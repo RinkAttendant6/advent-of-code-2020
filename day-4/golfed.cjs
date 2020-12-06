fe240549fbdb8620ec2121bb68fc01b52e4282ba
Vincent Diep <vincent.diep61@gmail.com>
2020-12-06 06:30:18 -0500

    [golf] Combine regex and check for existence of optional groups for part 2

--- day-4/golfed.cjs
+++ day-4/golfed.cjs
@@ -1,4 +1,4 @@
-d=require('fs').readFileSync(__dirname+`/input.txt`,{encoding:'utf8'}).split`
+r=[/\bbyr:(19[2-9]\d|200[0-2](\s|$))?/,/\biyr:(20(1\d|20)(\s|$))?/,/\beyr:(20(2\d|30)(\s|$))?/,/\bhgt:(1([5-8]\d|9[0-3])cm|(59|6\d|7[0-6])in(\s|$))?/,/\bhcl:(#[\da-f]{6}(\s|$))?/,/\becl:((amb|blu|brn|gr[ny]|hzl|oth)(\s|$))?/,/\bpid:(\d{9}(\s|$))?/]
+console.log(require('fs').readFileSync(__dirname+`/input.txt`,{encoding:"utf8"}).split`
 
-`
-console.log(d.filter(_=>[/\bbyr:/,/\biyr:/,/\beyr:/,/\bhgt:/,/\bhcl:/,/\becl:/,/\bpid:/].every($=>$.test(_))).length,d.filter(_=>[/\bbyr:(19[2-9]\d|200[0-2])\b/,/\biyr:20(1\d|20)\b/,/\beyr:20(2\d|30)\b/,/\bhgt:(1([5-8]\d|9[0-3])cm|(59|6\d|7[0-6])in)\b/,/\bhcl:#[\da-f]{6}\b/,/\becl:(amb|blu|brn|gr[ny]|hzl|oth)\b/,/\bpid:\d{9}\b/].every($=>$.test(_))).length)
+`.reduce(([x,y],_)=>[x+r.every($=>$.test(_)),y+r.every($=>$.exec(_)?.[1])],[0,0]))
