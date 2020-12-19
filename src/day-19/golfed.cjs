73698ec5b3e20753a23739d93f20cc6de3100c8a
Vincent Diep <vincent.diep61@gmail.com>
2020-12-19 17:11:13 -0500

    [golf] Day 19 solution

---
+++ src/day-19/golfed.cjs
@@ -0,0 +1,9 @@
+[C,D]=require('fs').readFileSync(process.argv[2],{encoding:"utf8"}).trim()[S=`split`]`
+
+`
+X=[]
+C[S]`
+`.map(c=>([i,s]=c[S]`:`,X[i]=s[S]` | `.map(g=>g[S]` `.map(eval))))
+E=r=>r.map(t=>t+0!=t?t:`(${X[t].map(E).join`|`})`).join``
+console.log(D.match(RegExp(`^${E([8,11])}$`,'gm')).length,D[S]`
+`.filter(m=>[1,2,3,4].some(d=>RegExp(`^${E([8])}+?${E([42]).repeat(d)+E([31]).repeat(d)}$`).test(m))).length)
